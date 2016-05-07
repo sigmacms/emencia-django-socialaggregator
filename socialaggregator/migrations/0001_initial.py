28819a99d835cf0cc6b8185aa91ed2991f210f5b
bashu <basil.shubin@gmail.com>
2016-05-07 12:54:10 +0600

    combine all migrations in one

--- socialaggregator/migrations/0001_initial.py
+++ socialaggregator/migrations/0001_initial.py
@@ -2,8 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import datetime
-import filebrowser.fields
+import filer.fields.image
 import taggit.managers
 
 
@@ -11,6 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('taggit', '0002_auto_20150616_2121'),
+        ('filer', '0002_auto_20150606_2003'),
     ]
 
     operations = [
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=250, verbose_name='name')),
                 ('query', models.CharField(max_length=250, verbose_name='query')),
-                ('social_plugin', models.CharField(max_length=250, verbose_name='social plugin', choices=[(b'edsa_wordpress_rss', b'Wordpress RSS'), (b'edsa_facebook_fanpage', b'Facebook Fanpage'), (b'edsa_youtube_search', b'Youtube search'), (b'edsa_instagram', b'Instagram'), (b'edsa_twitter', b'Twitter')])),
+                ('social_plugin', models.CharField(max_length=250, verbose_name='social plugin', choices=[(b'edsa_facebook_fanpage', b'Facebook Fanpage'), (b'edsa_wordpress_rss', b'Wordpress RSS'), (b'edsa_linkedin', b'LinkedIn Company'), (b'edsa_youtube_search', b'Youtube search'), (b'edsa_instagram', b'Instagram'), (b'edsa_twitter', b'Twitter')])),
                 ('creation_date', models.DateTimeField(auto_now_add=True, verbose_name='creation date')),
                 ('slug', models.SlugField(unique=True, max_length=100, verbose_name='slug')),
             ],
@@ -52,8 +52,6 @@ class Migration(migrations.Migration):
                 ('slug', models.SlugField(unique=True, max_length=100, verbose_name='slug')),
                 ('description', models.TextField(verbose_name='description', blank=True)),
                 ('short_description', models.TextField(verbose_name='short description', blank=True)),
-                ('image', filebrowser.fields.FileBrowseField(default=None, max_length=255, null=True, verbose_name='image', blank=True)),
-                ('thumbnail', filebrowser.fields.FileBrowseField(default=None, max_length=255, null=True, verbose_name='thumbnail', blank=True)),
                 ('media_url', models.URLField(max_length=500, verbose_name='media url', blank=True)),
                 ('media_url_type', models.CharField(blank=True, max_length=100, verbose_name='media url type', choices=[(b'url', b'url'), (b'image', b'image'), (b'video', b'video')])),
                 ('priority', models.IntegerField(default=100, verbose_name='display priority')),
@@ -62,7 +60,7 @@ class Migration(migrations.Migration):
                 ('language', models.CharField(max_length=2, verbose_name='language', blank=True)),
                 ('ressource_date', models.DateTimeField(verbose_name='ressource date')),
                 ('social_id', models.CharField(max_length=250, verbose_name='social_id', blank=True)),
-                ('social_type', models.CharField(default=b'edsa_article', max_length=250, verbose_name='social plugin', choices=[(b'edsa_article', b'Article'), (b'edsa_wordpress_rss', b'Wordpress RSS'), (b'edsa_facebook_fanpage', b'Facebook Fanpage'), (b'edsa_youtube_search', b'Youtube search'), (b'edsa_instagram', b'Instagram'), (b'edsa_twitter', b'Twitter')])),
+                ('social_type', models.CharField(default=b'edsa_article', max_length=250, verbose_name='social plugin', choices=[(b'edsa_article', b'Article'), (b'edsa_facebook_fanpage', b'Facebook Fanpage'), (b'edsa_wordpress_rss', b'Wordpress RSS'), (b'edsa_linkedin', b'LinkedIn Company'), (b'edsa_youtube_search', b'Youtube search'), (b'edsa_instagram', b'Instagram'), (b'edsa_twitter', b'Twitter')])),
                 ('query', models.CharField(max_length=250, verbose_name='query', blank=True)),
                 ('favorite', models.BooleanField(default=False, verbose_name='favorite')),
                 ('view_size', models.CharField(default=b'default', max_length=100, verbose_name='view size', choices=[(b'default', b'default'), (b'xsmall', b'Xsmall'), (b'small', b'small'), (b'medium', b'medium'), (b'large', b'large'), (b'xlarge', b'Xlarge')])),
@@ -71,11 +69,13 @@ class Migration(migrations.Migration):
                 ('button_color', models.CharField(default=b'black', max_length=100, verbose_name='button color', choices=[(b'white', b'white'), (b'black', b'black'), (b'primary', b'primary'), (b'secondary', b'secondary'), (b'tertiary', b'tertiary')])),
                 ('background_color', models.CharField(max_length=250, verbose_name='background color', blank=True)),
                 ('new_page', models.BooleanField(default=False, verbose_name='open in new page')),
-                ('creation_date', models.DateTimeField(default=datetime.datetime(2016, 3, 19, 7, 13, 59, 358979), verbose_name='creation date', editable=False)),
+                ('creation_date', models.DateTimeField(auto_now=True, verbose_name='creation date')),
                 ('update_date', models.DateTimeField(default=None, verbose_name='update date')),
                 ('updated', models.BooleanField(default=False, verbose_name='updated')),
                 ('feeds', models.ManyToManyField(to='socialaggregator.Feed', verbose_name='feeds')),
+                ('image', filer.fields.image.FilerImageField(default=None, to='filer.Image', max_length=255, blank=True, null=True, verbose_name='image')),
                 ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', blank=True, help_text='A comma-separated list of tags.', verbose_name='Tags')),
+                ('thumbnail', filer.fields.image.FilerImageField(related_name='+', default=None, to='filer.Image', max_length=255, blank=True, null=True, verbose_name='thumbnail')),
             ],
             options={
                 'ordering': ('-priority', 'name'),
